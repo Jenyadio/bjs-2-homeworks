a70bf87f484184e7cf5faf908b34d76bc7370dd0
jenyadio <jenyadio@gmail.com>
2021-10-17 11:18:14 +0300

    Task-1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,20 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let D = b * b - 4 * a * c;
+  let arr = [];
+  let x1 = 0;
+  let x2 = 0;
+
+  if (D < 0) {
+    return arr;
+  } else if (D === 0) {
+    x1 = -b / (2 * a);
+    arr.push(x1);
+  } else if (b ** 2 - 4 * a * c > 0) {
+    x1 = (-b + Math.sqrt(D)) / (2 * a); 
+    x2 = (-b - Math.sqrt(D)) / (2 * a); 
+    arr.push(x1, x2);
+  }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
