8b10078f919cce7a1e4bab5b91ae807b1377c91a
jenyadio <jenyadio@gmail.com>
2021-10-28 22:45:34 +0300

    Object homework done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,33 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (mark1, mark2, ...mark3) {
+  if(this.marks === undefined) {
+    this.marks = [mark1, mark2, ...mark3];
+  }
+}
+
+Student.prototype.getAverage = function () {
+   return this.marks.reduce((a, b) => (a + b)) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
