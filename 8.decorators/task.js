7b1fdbd4d9c5ce81292c31df18b38704af0de690
jenyadio <jenyadio@gmail.com>
2021-11-17 22:34:42 +0300

    Decorator homework

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,44 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  
+  function wrapper(...args) {
+      const hash = args.join(","); 
+      let idx = cache.findIndex((item) => item.hash === hash); 
+      if (idx !== -1 ) { 
+        // console.log("Из кэша: " + cache[idx].result); 
+        return "Из кэша: " + cache[idx].result;
+      }
+  
+      let result = func(...args);
+      cache.push({hash, result}) ; 
+      if (cache.length > 5) { 
+        cache.shift(); 
+      }
+      // console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+  }
+  return wrapper;
+}
+  
+
+function debounceDecoratorNew(func, ms) {
+  let timeoutFlag = false;
+  return function() {
+    if (timeoutFlag) return;
+    func.apply(this, arguments);
+    timeoutFlag = true;
+    setTimeout(() => timeoutFlag = false, ms);
+  };
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
-
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let timeoutFlag = false;
+  function wrapper(...args) {
+    if (timeoutFlag) return;
+    func.apply(this, arguments);
+    timeoutFlag = true;
+    setTimeout(() => {timeoutFlag = false; wrapper.count++}, ms);
+  };
+  wrapper.count = 0;
+  return wrapper;
 }
